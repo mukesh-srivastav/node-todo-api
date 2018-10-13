494b6bf8bb131e6b88917e3a03948c3d861f96ed
Mukesh Srivastava <mukeshsrivastav691@gmail.com>
2018-10-13 16:55:00 +0530

    Separate test and developement environment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-var localUrl = 'mongodb://localhost:27017/TodoApp';
+// var localUrl = 'mongodb://localhost:27017/TodoApp';
 
-mongoose.connect(process.env.MONGODB_URI || process.env.PROD_MONGODB || localUrl);
-
+// mongoose.connect(process.env.MONGODB_URI || process.env.PROD_MONGODB || localUrl);
+mongoose.connect(process.env.MONGODB_URI);
 module.exports = {
 	mongoose
 };
