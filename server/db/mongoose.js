6700771984342948cd70e02ff521af2dc09e7334
Mukesh Srivastava <mukeshsrivastav691@gmail.com>
2018-10-13 14:11:42 +0530

    Reinitiate DB connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 var localUrl = 'mongodb://localhost:27017/TodoApp';
+
 mongoose.connect(process.env.MONGODB_URI || process.env.PROD_MONGODB);
 
 module.exports = {
