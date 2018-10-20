56b525e9ce675fedf800e75d3ffcf965533c8276
Mukesh Srivastava <mukeshsrivastav691@gmail.com>
2018-10-20 15:21:43 +0530

    Add populate users in TestDatabase with token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,22 +4,10 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
-const todos = [{
-	_id: new ObjectID(),
-	text: 'First test task'
-}, {
-	_id: new ObjectID(),
-	text: 'Second test task',
-	completed: true,
-	completedAt: 3
-}];
-
-beforeEach((done) => {
-	Todo.remove({}).then(() => {
-		return Todo.insertMany(todos);
-	}).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
 	it('should create a new todo record', (done) => {
