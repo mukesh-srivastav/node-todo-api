5846f9a2b6857e45f35ea82780c85ec677b80336
Mukesh Srivastava <mukeshsrivastav691@gmail.com>
2018-10-21 01:07:21 +0530

    Add DELETE /users/me/token. Logout functionality

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,34 @@ UserSchema.methods.generateAuthToken = function () {
 	});
 };
 
+UserSchema.methods.removeToken = function (token) {
+	var User = this;
+
+	return User.update({
+		$pull: {
+			tokens: {token}
+		}
+	})
+};
+
+UserSchema.statics.findByCredentials = function (email, password) {
+	var User = this;
+
+	return User.findOne({email}).then((user) => {
+		if(!user)
+			return Promise.reject();
+
+		return new Promise((resolve, reject) => {
+			bcrypt.compare(password, user.password, (err, res) => {
+				if (res) {
+					resolve(user);
+				} else {
+					reject();
+				}
+			});
+		});
+	});
+};
 
 UserSchema.statics.findByToken = function (token) {
 	var User = this;
@@ -84,25 +112,6 @@ UserSchema.pre('save', function (next) {
 		next();
 	}
 });
-
-UserSchema.statics.findByCredentials = function (email, password) {
-	var User = this;
-
-	return User.findOne({email}).then((user) => {
-		if(!user)
-			return Promise.reject();
-
-		return new Promise((resolve, reject) => {
-			bcrypt.compare(password, user.password, (err, res) => {
-				if (res) {
-					resolve(user);
-				} else {
-					reject();
-				}
-			});
-		});
-	});
-};
 
 var User = mongoose.model('User', UserSchema);
 
